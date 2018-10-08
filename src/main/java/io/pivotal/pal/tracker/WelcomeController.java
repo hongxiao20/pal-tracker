57b9909848624aafe91db65f2fcb3fe88c2de1c5
agullipa@ford.com <agullipa@ford.com>
2018-10-08 17:07:34 -0400

    Environment Variable Lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private final String greeting;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String greeting) {
+        this.greeting = greeting;
+    }
+
     @GetMapping("/")
-    private String sayHello() {
-        return "hello";
+    public String sayHello() {
+        return greeting;
     }
 }
